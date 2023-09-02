14924a32d3a3f68804881d4b1fc737c6fd4adfd0
Lars Jørgensen <lj@csep.dk>
2023-09-02 02:19:39 +0200

    Fixed get all rooms fetch

--- Frontend/drawing-queens/app/rooms/rooms.js
+++ Frontend/drawing-queens/app/rooms/rooms.js
@@ -1,51 +1,21 @@
 "use client";
-import React, { useState, useEffect } from "react";
-export default function Rooms() {
-  const [rooms, setRooms] = useState([]);
-  useEffect(() => {
-    // GET request using fetch inside useEffect React hook
-    fetch("https://localhost:7157/api/Room")
-      .then((response) => response.json())
-      .then((data) => setRooms(data));
-    console.log(rooms);
+import { useEffect, useState } from 'react'
 
-    // empty dependency array means this effect will only run once (like componentDidMount in classes)
-  }, []);
-  return <h1>fff</h1>;
+const App = () => {
+    const [rooms, setRooms] = useState([])
+    useEffect(() => {
+        (async () => {
+            const data = await fetch('https://localhost:7157/api/Room')
+                .then(res => res.json())
+            setRooms(data);
+        })()
+    }, [])
+    
+    return (<ul>
+      {rooms.map(room => (
+        <li key={room.name}>{room.name}</li>
+      ))}
+    </ul>)
 }
 
-// import Page from "../page";
-
-/* function Rooms() {
-  const [rooms, setRooms] = useState([]);
-  useEffect(() => {
-    async function getRooms() {
-      const response = await fetch("https://localhost:7157/api/Room");
-      const data = await response.json();
-      setRooms(data);
-      console.log(data);
-      console.log(rooms);
-    }
-    getRooms();
-
-    console.log(rooms);
-  }, []);
-}
-
-export default Rooms; */
-
-/* export default function Rooms() {
-  const [rooms, setRooms] = useState([]);
-  useEffect(() => {
-    async function getRooms() {
-      const response = await fetch("https://localhost:7157/api/Room");
-      const data = await response.json();
-      setRooms(data);
-      console.log(data);
-      console.log(rooms);
-    }
-    getRooms();
-
-    console.log(rooms);
-  }, []);
-} */
+export default App
